ccd946f4c60a3410023bacfb18d48f659089dbd3
manuel <ibeanonymous@protonmail.com>
2021-01-03 19:56:55 +0000

    added context and other minor changes

--- ft_classifier.py
+++ ft_classifier.py
@@ -1,6 +1,5 @@
 import fasttext as ft
 from utils import change_dict, read_csv, write_lines
-import os
 from pathlib import Path
 
 ft.FastText.eprint = lambda x: None  # suppress a deprecation warning
@@ -9,21 +8,46 @@ DATA_PATH = Path('data')
 MODEL_FILEPATH = DATA_PATH / 'ft_classifier.bin'
 TRAIN_FT_FILEPATH = DATA_PATH / 'train.ft'
 
+CONTEXT_KEY = 'context'
 LABEL_KEY = 'class'
 TEXT_KEY = 'utterance'
 
-PREFIX = '__label__'
+CONTEXT_PREFIX = '__context__'
+LABEL_PREFIX = '__label__'
 
 
-class TrainedModelNotFound(Exception):
+class InputFileNotFoundError(Exception):
+    pass
+
+
+class TrainingDataNotFoundError(Exception):
+    pass
+
+
+class TrainedModelNotFoundError(Exception):
     pass
 
 
 # `model` contains the last trained model
 # if one exists then it is loaded when this file is imported
 model = None
-if os.path.exists(MODEL_FILEPATH):
+if MODEL_FILEPATH.exists():
     model = ft.load_model(str(MODEL_FILEPATH))
+
+
+def _clean_context(text, context_prefix=CONTEXT_PREFIX):
+    text = text.strip().lower()
+    if len(text) > 0:
+        text = context_prefix + text.replace(' ', '_')
+    return text
+
+
+def _clean_contexts(data, context_key=CONTEXT_KEY,
+                    context_prefix=CONTEXT_PREFIX):
+    if context_key in data[0].keys():
+        data = [change_dict(row, context_key, _clean_context(
+            row[context_key], context_prefix=context_prefix)) for row in data]
+    return data
 
 
 def _clean_labels(data, label_key=LABEL_KEY, relevant_labels=None):
@@ -54,7 +78,10 @@ def _clean_texts(data, text_key=TEXT_KEY):
 
 
 def _load_data(filename, text_key=TEXT_KEY, remove_empty=True):
-    data = read_csv(filename)
+    try:
+        data = read_csv(filename)
+    except FileNotFoundError:
+        raise InputFileNotFoundError
 
     # add other conditions if needed
     # currently only ignores rows with no utterance
@@ -66,37 +93,52 @@ def _load_data(filename, text_key=TEXT_KEY, remove_empty=True):
 
 
 def _write_ft_file(filename, text_key=TEXT_KEY, label_key=LABEL_KEY,
-                   remove_empty=True, clean=True, relevant_labels=None,
-                   prefix=PREFIX):
+                   context_key=CONTEXT_KEY, remove_empty=True, clean=True,
+                   relevant_labels=None, label_prefix=LABEL_PREFIX,
+                   context_prefix=CONTEXT_PREFIX):
     data = _load_data(filename, text_key=text_key, remove_empty=remove_empty)
 
     if clean:
         data = _clean_texts(data, text_key=text_key)
         data = _clean_labels(data, label_key=label_key,
                              relevant_labels=relevant_labels)
+        data = _clean_contexts(data, context_key=context_key,
+                               context_prefix=context_prefix)
 
     def add_prefix(label):
-        return f"{prefix}{label if len(label) > 0 else 'unknown'}"
+        return f"{label_prefix}{label if len(label) > 0 else 'unknown'}"
 
-    write_lines(
-        [' '.join([add_prefix(row[label_key]), row[text_key]]) for row in data],
-        TRAIN_FT_FILEPATH
-    )
+    def make_row(label, text, context):
+        row = [add_prefix(label)] + ([context] if context else []) + [text]
+        return ' '.join(row)
+
+    TRAIN_FT_FILEPATH.parent.mkdir(parents=True, exist_ok=True)
+
+    texts = [make_row(row[label_key],
+                      row[context_key] if context_key in row.keys() else '',
+                      row[text_key]) for row in data]
+    write_lines(texts, TRAIN_FT_FILEPATH)
 
 
 def train(filename, text_key=TEXT_KEY, label_key=LABEL_KEY, remove_empty=True,
-          clean=True, relevant_labels=None, prefix=PREFIX, multilabel=False):
-    _write_ft_file(filename, text_key=text_key, label_key=label_key,
-                   remove_empty=remove_empty, clean=clean,
-                   relevant_labels=relevant_labels, prefix=prefix)
+          clean=True, relevant_labels=None, prefix=LABEL_PREFIX,
+          preprocess=True, multilabel=False):
+    if preprocess:
+        _write_ft_file(filename, text_key=text_key, label_key=label_key,
+                       remove_empty=remove_empty, clean=clean,
+                       relevant_labels=relevant_labels, label_prefix=prefix)
 
     global model
 
-    # hyperparameters might need to be adjusted
-    # https://fasttext.cc/docs/en/python-module.html
+    # TODO: hyperparameters might need to be adjusted
+    #  https://fasttext.cc/docs/en/python-module.html
+
+    if not TRAIN_FT_FILEPATH.exists():
+        raise TrainingDataNotFoundError
+
     model = ft.train_supervised(
         input=str(TRAIN_FT_FILEPATH),
-        minCount=2,
+        minCount=5,
         loss='ova' if multilabel else 'softmax'
     )
 
@@ -107,7 +149,7 @@ def train(filename, text_key=TEXT_KEY, label_key=LABEL_KEY, remove_empty=True,
 def predict(text):
     try:
         labels, probs = model.predict(_clean_text(text), k=-1)
-        labels = [label.split(PREFIX)[1] for label in labels]
+        labels = [label.split(LABEL_PREFIX)[1] for label in labels]
         return dict(zip(labels, probs))
     except AttributeError:
-        raise TrainedModelNotFound
+        raise TrainedModelNotFoundError
