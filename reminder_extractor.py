8bd822f27bf4eb05747a23734549e725ac94ede2
manuel <ibeanonymous@protonmail.com>
2021-01-09 17:37:32 +0000

    improved grammar/spellchecker of reminder_extractor.py

--- reminder_extractor.py
+++ reminder_extractor.py
@@ -1,15 +1,48 @@
 import nltk
+import re
+from spellchecker import SpellChecker
 
-REGEX = "\\b[0-9a-zA-Z]{1,}\\b"  # TODO: Update this for punctuation
+FORBIDDENWORDS = ['reminders', 'reminder', 'reminds', 'remind']
+REGEX = "\\b[0-9a-zA-Z']{1,}\\b"  # TODO: Update this for punctuation
+
+spell = SpellChecker()
+
+# TODO fix case
+#  https://towardsdatascience.com/truecasing-in-natural-language-processing-12c4df086c21  # noqa
+
+# TODO find named entities (use spacy)
 
 # TODO: Improve grammar
+#  Run `nltk.help.upenn_tagset()` to check tags or go to
+#  https://www.guru99.com/pos-tagging-chunking-nltk.html
 GRAMMAR = r"""
+    J:
+        {<JJ|JJR|JJS>*}
+    N:
+        {<NN|NNS|NNP|NNPS>*}
+    P:
+        {<PRP|PRP\$>*}
+    R:
+        {<RB|RBR|RBS|RP>*}
+    V:
+        {<VB|VBG|VBD|VBD|VBN|VBP|VBZ>*}
     NBAR:
-        {<PRP$|DT><NN.*|JJ|JJR>*<NN.*>}  
-        {<NN.*|JJ|JJR>*<NN.*>}  
+        {<P|DT>*<N.*|J.*|>*<N.*|VBG>}
+    NBAREXT:
+        {<CC|IN><NBAR>}
     NP:
-        {<NBAR><IN><NBAR>}
-        {<NBAR>}
+        {<NBAR|NBAREXT>*}
+    VBAR:
+        {<V><V>*<R>*<TO>*}
+        {<V><V>*<R>*<TO>*}
+        {<V><V>*<R|TO>*}
+    ACTION:
+        {<VBAR.*|NP.*|R.*>*}
+    VP:
+        {<ACTION><IN.*|CC.*|ACTION.*>*}
+        {<ACTION>}
+"""
+__GRAMMAR = r"""
     ACT:
         {<VB|VBP|VBD>*<NP><VBD|VBN>}
         {<VB|VBP|VBD>*<NP><VBD|VBN>}
@@ -28,12 +61,19 @@ def _pos_tag(text):
 
 
 def _extractor(text):
+    text = [spell.correction(w) for w in text.split()]
+    if text[0] in FORBIDDENWORDS:
+        text = text[1:]
+    if text[-1] in FORBIDDENWORDS:
+        text = text[:-1]
+    text = ' '.join(text)
     toks = nltk.regexp_tokenize(text, REGEX)
     postoks = nltk.tag.pos_tag(toks)
     tree = chunker.parse(postoks)
     extractions = [' '.join(list(zip(*leaf))[0]) for leaf in _leaves(tree)]
     # TODO: drop extractions such as 'remind' 'reminder'
-    # print(text, [t for t in tree])
+    extractions = [e for e in extractions if not any(w in e for w in FORBIDDENWORDS)]
+    extractions = re.sub(' +', ' ', ' '.join(extractions)).strip()
     return extractions
 
 
